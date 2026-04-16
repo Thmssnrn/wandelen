eabe146fd239ad960e6058c058cee8e2062bdd1d
Thmssnrn <verzamelt.schartong_0u@icloud.com>
2026-04-16 19:56:05 +0200

    Update service worker to handle fetch events

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('v1').then(cache =>
-      cache.addAll(['/', '/index.html'])
+      cache.addAll([
+        './',
+        './index.html'
+      ])
     )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
